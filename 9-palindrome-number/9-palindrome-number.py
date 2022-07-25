c1223782504a168e8ee976790549322d20de46fd
wanning <81507755+incalescence@users.noreply.github.com>
2022-07-25 11:38:25 +1000

    Time: 106 ms (40.31%), Space: 14 MB (16.33%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        string = str(x)
+        reverse = string[::-1]
+        if string == reverse:
+            return True
+        return False
