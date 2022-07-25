0cf3ecfa311ffd8fcfa532e950f6cdb710c0c6bf
wanning <81507755+incalescence@users.noreply.github.com>
2022-07-25 11:29:29 +1000

    Time: 76 ms (51.68%), Space: 14.4 MB (66.69%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+     
+        data_s = {}
+        for i in s:
+            if i not in data_s:
+                data_s[i]=0
+            else:
+                data_s[i]+=1
+        data_t = {}
+        for i in t:
+            if i not in data_t:
+                data_t[i]=0
+            else:
+                data_t[i]+=1
+        
+        if len(s) == len(t) and data_s == data_t:
+            return True
+        return False
+                
+            
+        
