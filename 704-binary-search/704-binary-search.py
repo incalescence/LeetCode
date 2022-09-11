3a8c067953fd2ecf02ded08e39f4cb1725488676
wanning <81507755+incalescence@users.noreply.github.com>
2022-09-12 09:33:42 +1000

    Time: 670 ms (5.02%), Space: 15.5 MB (73.41%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,17 +1,13 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        low, high = 0, len(nums) - 1
-        while(low <= high):
-            mid = (low + (high - low)//2)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums)-1
+        while high >= low:
+            mid = (low+high)//2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 low = mid + 1
             else:
                 high = mid - 1
-        return -1
+        return -1 
