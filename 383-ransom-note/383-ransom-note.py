9eb9d391c8657bd779122c2a491cf0add7b21e6c
wanning <81507755+incalescence@users.noreply.github.com>
2022-09-19 20:40:24 +1000

    Time: 83 ms (60.61%), Space: 14.1 MB (52.99%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,17 @@
+from collections import Counter 
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        note_dict = Counter(ransomNote)
+        magazine_dict = Counter(magazine)
+        
+        magazine_dict.subtract(note_dict)
+        
+        if any(value < 0 for value in magazine_dict.values()):
+            return False
+        
+        return True 
+                
+
+        
+        
