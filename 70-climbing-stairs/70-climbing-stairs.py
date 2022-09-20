7d1ee559b4efdfa940627ad93864331fed3ba705
wanning <81507755+incalescence@users.noreply.github.com>
2022-09-20 17:53:25 +1000

    Time: 24 ms (99.18%), Space: 14 MB (11.78%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:  
+    @cache
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        else:
+            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        
+            
+        
