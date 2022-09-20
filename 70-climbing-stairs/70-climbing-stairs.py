e57d32f2d64f5bdc44161ef182ea39ec6d0ab79d
wanning <81507755+incalescence@users.noreply.github.com>
2022-09-20 17:54:15 +1000

    Time: 59 ms (20.54%), Space: 13.9 MB (57.09%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,9 +2,9 @@ class Solution:
     @cache
     def climbStairs(self, n: int) -> int:
         #base cases
-        if n == 1:
+        if (n == 1):
             return 1
-        if n == 2:
+        elif (n == 2):
             return 2
         else:
             return self.climbStairs(n - 1) + self.climbStairs(n - 2)
