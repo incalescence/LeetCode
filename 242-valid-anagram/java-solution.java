7c5ace9de74b34eee90cf7cdc3659f7f70070495
wanning <81507755+incalescence@users.noreply.github.com>
2022-11-19 09:37:58 +1100

    Create java-solution.java

---
+++ 242-valid-anagram/java-solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // check the lengths 
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        // change s and t to 
+        char[] sArray=s.toCharArray();
+        char[] tArray=t.toCharArray();  
+
+        // have an array of all the possible characters 
+        int[] alphabetBuffer = new int[128];
+
+        // go through the first string increment the character array
+        for (char c : sArray) {
+            alphabetBuffer[c] += 1;
+        }
+
+        // decrease going through the second string
+        for (char c : tArray) {
+            alphabetBuffer[c] -= 1;
+            if (alphabetBuffer[c] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
