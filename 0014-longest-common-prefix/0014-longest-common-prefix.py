1bbe2a4493ec7895cfa513ef827a20c6963c388e
wanning <81507755+incalescence@users.noreply.github.com>
2023-04-02 13:20:49 +1000

    Time: 36 ms (58.51%), Space: 14 MB (5.34%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # empty array 
+        if len(strs) == 0:
+            return ''
+        
+        prefix = strs[0]
+        
+        for i in range(1, len(strs)):
+            if len(strs[i]) < len(prefix):
+                prefix = prefix[0:len(strs[i])]
+            for j in range(len(prefix)):
+                if prefix[j] != strs[i][j]:
+                    if j == 0:
+                        prefix = ''
+                    else:
+                        prefix = prefix[0:j]
+                    break
+                if j == len(strs[i])-1:
+                    prefix = strs[i]
+                    break
+        return prefix
+
+            
+            
+        
