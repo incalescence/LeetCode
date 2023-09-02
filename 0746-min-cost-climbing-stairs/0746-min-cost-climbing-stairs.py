05cfd33db5f8482f42542d2ecf7e930e3d035f4a
wanning <81507755+incalescence@users.noreply.github.com>
2023-09-02 19:17:11 +1000

    Time: 61 ms (73.16%), Space: 16.5 MB (65.05%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,10 +1,6 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         DP = []
-        # subproblem: minimum cost to get to that step 
-        # recursive case: dp[i] = cost[i] + min(dp[i-2], dp[i-1])
-        # base case: cost to get to step1 or step 2
-        
         for i in range(len(cost)):
             if i == 0 or i == 1:
                 DP.append(cost[i])
